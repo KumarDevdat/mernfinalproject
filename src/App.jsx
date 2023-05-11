b19f82142aeb151cc7dd9911f54d000f437aa17e
Devdat Kumar <Devdat_kumar@outlook.com>
2023-05-11 21:09:33 +0500

    testing vite, mern, and tailwind componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Counter from './components/counter'
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Counter />
     </>
   );
 }
