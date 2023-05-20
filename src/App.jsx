44a3db3fd14900990513ab37cb932a9129bf9f6f
Touqeer <tzain3940@gmail.com>
2023-05-20 19:13:36 +0500

    small changes in some files packages and index files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import Counter from './components/counter'
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Counter />
+   
+    <Home/>
     </>
   );
 }
