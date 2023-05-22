386af2271277759945c27629529f0dc46c1af39f
Touqeer <tzain3940@gmail.com>
2023-05-22 19:07:56 +0500

    Responsive Landing Page Brochure section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,22 @@
 import menu  from "./menu.svg";
-import close from "./close.svg";
+// import close from "./close.svg";
 import cycle  from "./cycle.gif";
 import hike  from "./hike.gif";
 import running  from "./running.gif";
+import yoga from "./yoga.png"
+import pulsemeter from "./pulsemeter.png"
+import strategies from "./strategies.png"
+import workout from "./workout.png"
+
 
 export {
     menu,
-    close,
+    // close,
     cycle,
     hike,
     running,
+    yoga,
+    strategies,
+    workout,  
+    pulsemeter
 };
